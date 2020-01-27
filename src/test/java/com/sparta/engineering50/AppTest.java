93fb2d8a3e87a62bdc7f19d6c318b8ebce255267
Tyler Smart <TSmart@spartaglobal.com>
2020-01-27 17:20:58 +0000

    Added tests for rabbit class

--- src/test/java/com/sparta/engineering50/AppTest.java
+++ src/test/java/com/sparta/engineering50/AppTest.java
@@ -1,20 +1,33 @@
 package com.sparta.engineering50;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class RabbitTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+    Rabbit rabbit = new Rabbit();
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void ageShouldBeZero() {
+        assertEquals( 0, rabbit.getAge());
+    }
+    @Test
+    public void stateShouldBeYoung() {
+        assertEquals("young",rabbit.getState());
+    }
+    @Test
+    public void genderShouldBeMaleOrFemale() {
+        assertEquals("male", rabbit.getGender()) || assertEquals("Female", rabbit.getGender());
+    }
+    @Test
+    public void rabbitCounterShouldBeOne() {
+        assertEquals(1,rabbit.getRabbitCounter());
+    }
+    @Test
+    public void stateShouldChangeToAdultAfterThreeMonths() {
+        Rabbit rabbit1 = new Rabbit();
+        for(int i; i<4; i++) {
+            rabbit1.increaseAge();
+        }
+        assertEquals("adult", rabbit1.getState());
     }
 }
