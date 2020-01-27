a8fee2bd44df05c9468003f898f9776609c4b49b
Jack Goodwin <jackgoodwin15@gmail.com>
2020-01-27 16:41:22 +0000

    Initial commit

---
+++ src/main/java/com/sparta/engineering50/App.java
@@ -0,0 +1,13 @@
+package com.sparta.engineering50;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
