1e7a1a812e008c4ea9e55b374990f74628c4fa99
Tyler Smart <tsmart@spartaglobal.com>
2020-01-28 14:41:52 +0000

    added breeding

--- src/main/java/com/sparta/engineering50/Field.java
+++ src/main/java/com/sparta/engineering50/Field.java
@@ -3,13 +3,33 @@ package com.sparta.engineering50;
 import java.util.ArrayList;
 
 public class Field {
-    private ArrayList<Rabbit> rabbits = new ArrayList<Rabbit>();
+    private ArrayList<Rabbit> rabbits = new ArrayList<>();
 
     public ArrayList<Rabbit> getRabbits() {
         return rabbits;
     }
 
-    public void createRabbit() {
-        rabbits.add(new Rabbit());
+    //method for testing
+    public void addRabbit(Rabbit rabbit) {
+        rabbits.add(rabbit);
+    }
+
+    public void addRabbits(ArrayList<Rabbit> rabbitArray) {
+        for(Rabbit rabbit:rabbitArray) {
+            addRabbit(rabbit);
+        }
+    }
+
+    public void breed() {
+        for (Rabbit rabbit : rabbits) {
+            if (rabbit.isAvailable() && rabbit.getGender().equals("male")) {
+                rabbit.setAvailable(false);
+                for (Rabbit rabbit1 : rabbits) {
+                    if (rabbit1.isAvailable() && rabbit1.getGender().equals("female")) {
+                        rabbit1.getPregnant();
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/java/com/sparta/engineering50/FieldTest.java
+++ src/test/java/com/sparta/engineering50/FieldTest.java
@@ -1,15 +1,40 @@
 package com.sparta.engineering50;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FieldTest {
-    private Field field = new Field();
     @Test
-    void addRabbitToArrayList() {
-        field.createRabbit();
-        assertEquals(1,field.getRabbits().size());
+    void testThatRabbitsCanGetPregnant() {
+        Field field = new Field();
+        Rabbit maleRabbit = new Rabbit();
+        maleRabbit.setGender("male");
+        Rabbit femaleRabbit = new Rabbit();
+        femaleRabbit.setGender("female");
+        for (int i = 0; i <= 3; i++) {
+            maleRabbit.increaseAge();
+            femaleRabbit.increaseAge();
+        }
+        field.addRabbit(maleRabbit);
+        field.addRabbit(femaleRabbit);
+        field.breed();
+        assertEquals("pregnant", femaleRabbit.getState());
+    }
+
+    @Test
+    public void testThatNewRabbitsAreAddedToRabbitArray () {
+        Rabbit rabbit = new Rabbit();
+        rabbit.setGender("female");
+        rabbit.getPregnant();
+        ArrayList<Rabbit> newRabbits = rabbit.giveBirth();
+        Field field = new Field();
+        field.addRabbits(newRabbits);
+        assertTrue(field.getRabbits().size()>0);
     }
 }
