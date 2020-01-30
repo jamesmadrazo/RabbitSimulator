1c8ac5a4983b43543a51f329817c046ad2bfa721
Tyler Smart <tsmart@spartaglobal.com>
2020-01-30 12:24:00 +0000

    removed 'state' and replaced with 'isPregnant'

--- src/main/java/com/sparta/engineering50/Timer.java
+++ src/main/java/com/sparta/engineering50/Timer.java
@@ -1,47 +1,47 @@
-package com.sparta.engineering50;
-
-import java.util.ArrayList;
-
-public class Timer {
-    //  private final int rabbitDead = 60;
-    //private int months = 0;
-
-    Rabbit rabbit = new Rabbit();
-
-    public static void ageRabbits(ArrayList<Rabbit> rabbits) {
-        for (Rabbit rabbit : rabbits) {
-            rabbit.increaseAge();
-            if (rabbit.getState().equals("dead")) {
-                RabbitCounter.deadCounterIncrease();
-                //  rabbits.remove()
-            }
-        }
-    }
-}
-   /*
-
-    Rabbit rabbit = new Rabbit();
-    Random rand = new Random();
-    int n = rand.nextInt(14) + 1;
-    int x = rand.nextInt(n) +1;
-    int y = n-x;
-
-    public int ifstam(int n, int x){
-        if (n + x >
-    }
-    private void babies() {
-
-        for (int i = 0; i < n; i++) {
-            Field.rabbits.add(new Rabbit());
-
-        }
-
-        for (int i = 0; i < x; i++) {
-            Field.rabbits.add(new Rabbit());
-
-        }
-
-    }
-*/
-
-
+//package com.sparta.engineering50;
+//
+//import java.util.ArrayList;
+//
+//public class Timer {
+//    //  private final int rabbitDead = 60;
+//    //private int months = 0;
+//
+//    Rabbit rabbit = new Rabbit();
+//
+//    public static void ageRabbits(ArrayList<Rabbit> rabbits) {
+//        for (Rabbit rabbit : rabbits) {
+//            rabbit.increaseAge();
+//            if (rabbit.getState().equals("dead")) {
+//                RabbitCounter.deadCounterIncrease();
+//                //  rabbits.remove()
+//            }
+//        }
+//    }
+//}
+//   /*
+//
+//    Rabbit rabbit = new Rabbit();
+//    Random rand = new Random();
+//    int n = rand.nextInt(14) + 1;
+//    int x = rand.nextInt(n) +1;
+//    int y = n-x;
+//
+//    public int ifstam(int n, int x){
+//        if (n + x >
+//    }
+//    private void babies() {
+//
+//        for (int i = 0; i < n; i++) {
+//            Field.rabbits.add(new Rabbit());
+//
+//        }
+//
+//        for (int i = 0; i < x; i++) {
+//            Field.rabbits.add(new Rabbit());
+//
+//        }
+//
+//    }
+//*/
+//
+//
