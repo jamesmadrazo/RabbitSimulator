fb377873f5ba4f3972b10066b569c08f37a49492
Tyler Smart <tsmart@spartaglobal.com>
2020-01-30 17:15:51 +0000

    fixed counter

--- src/main/java/com/sparta/engineering50/App.java
+++ src/main/java/com/sparta/engineering50/App.java
@@ -2,20 +2,9 @@ package com.sparta.engineering50;
 
 public class App {
     public static void main(String[] args) {
-
         System.out.println("Enter number of months");
         UserInput.getUserInput();
-        TimeSimulator timeSimulator = new TimeSimulator();
-        Rabbit maleRabbit = new Rabbit("male");
-        Rabbit femaleRabbit = new Rabbit("female");
-        Field.addRabbit(maleRabbit);
-        Field.addRabbit(femaleRabbit);
-
-        Fox maleFox = new Fox("male");
-        Fox femaleFox = new Fox("female");
-        FoxField.addFox(maleFox);
-        FoxField.addFox(femaleFox);
-        timeSimulator.initialiseTimeSimulator(UserInput.getUserI());
+        InitializeSimulator.beginSimulation(UserInput.getUserI());
     }
 
 
--- src/main/java/com/sparta/engineering50/InitializeSimulator.java
+++ src/main/java/com/sparta/engineering50/InitializeSimulator.java
@@ -1,4 +1,18 @@
 package com.sparta.engineering50;
 
 public class InitializeSimulator {
+    public static void beginSimulation(int seconds) {
+
+        TimeSimulator timeSimulator = new TimeSimulator();
+        Rabbit maleRabbit = new Rabbit("male");
+        Rabbit femaleRabbit = new Rabbit("female");
+        Field.addRabbit(maleRabbit);
+        Field.addRabbit(femaleRabbit);
+
+        Fox maleFox = new Fox("male");
+        Fox femaleFox = new Fox("female");
+        FoxField.addFox(maleFox);
+        FoxField.addFox(femaleFox);
+        timeSimulator.initialiseTimeSimulator(seconds);
+    }
 }
